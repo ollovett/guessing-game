5c0242032e491cd35ae75a187f3439072932479e
Owen Lovett <ollovett@cpp.edu>
2022-10-23 11:58:38 -0700

    init outline

---
+++ guessingGame.c
@@ -0,0 +1,27 @@
+// main
+    //while x = 1
+        //print menu
+            //1 to play
+            //2 to change number
+            //3 to quit
+        //ask user for choice
+
+        //if user selects 1
+            //generate random number within set range
+            //while user is wrong
+                //ask user to enter number
+                //check if equal
+                    //if equal tell user they won and return to menu
+                //check if lower or greater and tell user then loop
+        
+        //if user selects 2
+            //while valid number has not been selected
+            //tell user max value that can be set
+            //ask user for number
+            //check if valid
+                //if valid than set number and return number
+                //if not then tell user not valid and loop
+
+        //if user selects 3
+            // set x = 0 then end loop
+    //thank user for playing and end

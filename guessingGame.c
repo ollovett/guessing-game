9a098b4e80457a82f502ab5e7f8d7520e1a8a609
Owen Lovett <ollovett@cpp.edu>
2022-10-23 12:02:16 -0700

    init outline save file

--- guessingGame.c
+++ guessingGame.c
@@ -1,4 +1,9 @@
 // main
+
+    //check if file exists for max number
+        //if it exists set max number to that number
+        //if it does not exist, then default max number is 10
+    
     //while x = 1
         //print menu
             //1 to play
@@ -19,7 +24,7 @@
             //tell user max value that can be set
             //ask user for number
             //check if valid
-                //if valid than set number and return number
+                //if valid than set number, save to file, and return to menu
                 //if not then tell user not valid and loop
 
         //if user selects 3
